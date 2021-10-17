b1b355c5ab5260300c14683c1b02488c4e315845
Aliaksei Belski <aliaksei_belski@epicgames.com>
2021-10-17 16:52:09 -0700

    remove console

--- configs.js
+++ configs.js
@@ -2,10 +2,10 @@ const dotenv = require('dotenv');
 
 module.exports = async ({ options, resolveConfigurationProperty }) => {
   const envVars = dotenv.config({ path: options.stage === 'prod' ? '.env.prod' : '.env' }).parsed;
-  console.log('envVars', envVars);
+
   return Object.assign(
     {},
-    envVars,      // `dotenv` environment variables
-    process.env   // system environment variables
+    envVars, 
+    process.env
   );
 };
